8dd51302a65de085e9dd879574dd9b361a514992
HANIF <hanif.hidayatulloh@krl.co.id>
2024-01-02 12:38:28 +0700

    chat

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,19 +1,40 @@
-import React from 'react'
+import React, { useEffect } from 'react';
 
 const Footer = () => {
+    useEffect(() => {
+        const BASE_URL = "http://174.138.16.181:3000";
+
+        const script = document.createElement("script");
+        script.src = `${BASE_URL}/packs/js/sdk.js`;
+        script.defer = true;
+        script.async = true;
+
+        script.onload = () => {
+            window.chatwootSDK.run({
+                websiteToken: '7XwWVkt4acRPzJBpcEcbMi23',
+                baseUrl: BASE_URL
+            });
+        };
+
+        document.head.appendChild(script);
+
+        return () => {
+            // Cleanup script when the component unmounts
+            document.head.removeChild(script);
+        };
+    }, []); // Run only once on component mount
+
     return (
         <footer className='footer border border-t-[#33353F] border-l-transparent border-r-transparent text-white'>
             <div className='container p-12 flex justify-between'>
                 {/* Logo */}
-                <span>
-
-                </span>
+                <span></span>
                 <p className='text-slate-600'>
-                    All right reserved.
+                    All rights reserved.
                 </p>
             </div>
         </footer>
-    )
-}
+    );
+};
 
-export default Footer
+export default Footer;
